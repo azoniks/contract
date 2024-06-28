a7639d7ed2f44161497f6e819dc1d3dce8cf6ff2
Alexander Popov <alexander.popov@data-ox.com>
2024-06-28 17:18:52 +0300

    Changing the maximum length of input fields.

--- contracts_generator/forms.py
+++ contracts_generator/forms.py
@@ -8,19 +8,19 @@ class RentContractAdminForm(forms.ModelForm):
         model = RentContract
         fields = '__all__'
 
-    field_size = {'size': 50}
+    field_size = {'size': 60}
 
     landlord = forms.CharField(
-        max_length=40, widget=forms.TextInput(attrs=field_size), label='Vermieter')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Vermieter')
 
     renter = forms.CharField(
-        max_length=40, widget=forms.TextInput(attrs=field_size), label='Mieter')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Mieter')
 
     resident = forms.CharField(
-        max_length=30, widget=forms.TextInput(attrs=field_size), label='Wohnhaft')
+        max_length=50, widget=forms.TextInput(attrs=field_size), label='Wohnhaft')
 
     address = forms.CharField(
-        max_length=35, widget=forms.TextInput(attrs=field_size), label='Adresse')
+        max_length=50, widget=forms.TextInput(attrs=field_size), label='Adresse')
 
     persons = forms.CharField(
         max_length=200, widget=forms.Textarea(attrs={"cols": "40", "rows": "6"}), label='Personen')
@@ -37,10 +37,10 @@ class SaleContractAdminForm(forms.ModelForm):
     field_size = {'size': 60}
 
     customer = forms.CharField(
-        max_length=50, widget=forms.TextInput(attrs=field_size), label='Kunde')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Kunde')
 
     address = forms.CharField(
-        max_length=50, widget=forms.TextInput(attrs=field_size), label='Adresse')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Adresse')
 
     object = forms.CharField(
         max_length=70, widget=forms.Textarea(attrs={"cols": "40", "rows": "3"}), label='Objekt')
@@ -58,7 +58,7 @@ class BrokerSearchAdminForm(forms.ModelForm):
         fields = '__all__'
 
     customer = forms.CharField(
-        max_length=50, widget=forms.TextInput(attrs={'size': 55}), label='Kunde')
+        max_length=60, widget=forms.TextInput(attrs={'size': 60}), label='Kunde')
 
     location = forms.CharField(
         max_length=25, widget=forms.TextInput(attrs={'size': 30}), label='Ort')
@@ -71,13 +71,13 @@ class SearchContractAdminForm(forms.ModelForm):
 
     field_size = {'size': 60}
     customer = forms.CharField(
-        max_length=50, widget=forms.TextInput(attrs=field_size), label='Eigentümer')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Eigentümer')
 
     address_customer = forms.CharField(
-        max_length=50, widget=forms.TextInput(attrs=field_size), label='Adresse des Eigentümers')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Adresse des Eigentümers')
 
     address = forms.CharField(
-        max_length=50, widget=forms.TextInput(attrs=field_size), label='Adresse')
+        max_length=60, widget=forms.TextInput(attrs=field_size), label='Adresse')
 
     purchase_contract = forms.CharField(
         max_length=50, widget=forms.TextInput(attrs=field_size), label='Kaufvertrag', required=False)
